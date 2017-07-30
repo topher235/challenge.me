221693a8a5017e6ec5348233c596665392167efc
Topher <christopher.hurt.15@cnu.edu>
2017-07-29 17:30:38 -0700

    Implemented About activity

--- app/src/main/java/topher/challengeme/AboutActivity.java
+++ app/src/main/java/topher/challengeme/AboutActivity.java
@@ -1,6 +1,8 @@
 package topher.challengeme;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
 
 /**
  * Created by Christopher Hurt on 7/12/2017.
@@ -9,4 +11,33 @@ import android.app.Activity;
  */
 
 public class AboutActivity extends Activity {
+    private TextView message;
+    private String donationMsg = " I have \n" +
+            "made Challenge.Me free to use, but \n" +
+            "if you are enjoying the challenges \n" +
+            "and feel so inclined, I'd love you \n" +
+            "if you bought me a beer or coffee \n" +
+            "Thank you, you beautiful person!";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        message = (TextView) findViewById(R.id.aboutMessage);
+        setMessage();
+    }
+
+    private void setMessage() {
+        message.setText("Challenge.Me is an app made for "
+        + "the people who love to accept "
+        + "challenges. Whether you "
+        + "are practicing your over-achieving, "
+        + "go-getter attitude or you just want "
+        + "something to shake up life, "
+        + "Challenge.Me will reward those who "
+        + "stick with the challenges. You will have "
+        + "100 days of challenges to climb"
+        + "through the levels.");
+    }
 }
